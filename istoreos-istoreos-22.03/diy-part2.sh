e213875df0ffc6bdbb8bdec854470bba9a03efca
Lemon <149338935+Lemon1151@users.noreply.github.com>
2024-06-19 16:20:28 +0800

    Update diy-part2.sh

--- istoreos-istoreos-22.03/diy-part2.sh
+++ istoreos-istoreos-22.03/diy-part2.sh
@@ -29,53 +29,9 @@ cp -f $GITHUB_WORKSPACE/istoreos-istoreos-22.03/uboot/patches/105-fine3399-suppo
 cp -f $GITHUB_WORKSPACE/istoreos-istoreos-22.03/kernel/armv8.mk target/linux/rockchip/image/armv8.mk
 cp -f $GITHUB_WORKSPACE/istoreos-istoreos-22.03/kernel/patches/910-add-fine3399-dts.patch target/linux/rockchip/target/linux/rockchip/patches-5.10/910-add-fine3399-dts.patch
 
-# 网口配置为旁路由模式，注释下面三个网口模式替换命令后，网口模式会变成主路由模式。
-sed -i "s/armsom,p2pro)/armsom,p2pro|\\\\\n	rk3399,r08)/g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
-sed -i "s/rk3399,r08)/rk3399,r08|\\\\\n	rk3399,tpm312)/g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
-
-
-
-
-# 复制和修改u-boot压缩包SHA256校验码，编译失败时注意看是不是这个引起的。
-cp -f $GITHUB_WORKSPACE/configfiles/uboot_Makefile package/boot/uboot-rockchip/Makefile
-sha256_value=$(wget -qO- "https://github.com/xiaomeng9597/files/releases/download/u-boot-2021.07/u-boot-2021.07.tar.bz2.sha" | awk '{print $1}')
-if [ -n "$sha256_value" ]; then
-sed -i "s/.*PKG_HASH:=.*/PKG_HASH:=$sha256_value/g" package/boot/uboot-rockchip/Makefile
-fi
-cp -f $GITHUB_WORKSPACE/configfiles/u-boot.mk include/u-boot.mk
-
-
-
-
-
-# 创建rk3399 dts设备树文件夹
-mkdir -p target/linux/rockchip/dts/rk3399
-
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399.dtsi target/linux/rockchip/dts/rk3399/rk3399.dtsi
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-opp.dtsi target/linux/rockchip/dts/rk3399/rk3399-opp.dtsi
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-r08.dts target/linux/rockchip/dts/rk3399/rk3399-r08.dts
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-tpm312.dts target/linux/rockchip/dts/rk3399/rk3399-tpm312.dts
-
-
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399.dtsi target/linux/rockchip/armv8/files/arch/arm64/boot/dts/rockchip/rk3399.dtsi
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-opp.dtsi target/linux/rockchip/armv8/files/arch/arm64/boot/dts/rockchip/rk3399-opp.dtsi
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-r08.dts target/linux/rockchip/armv8/files/arch/arm64/boot/dts/rockchip/rk3399-r08.dts
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-tpm312.dts target/linux/rockchip/armv8/files/arch/arm64/boot/dts/rockchip/rk3399-tpm312.dts
-
-
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/rk3399.dtsi
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-opp.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/rk3399-opp.dtsi
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-r08.dts package/boot/uboot-rockchip/src/arch/arm/dts/rk3399-r08.dts
-cp -f $GITHUB_WORKSPACE/configfiles/rk3399-tpm312.dts package/boot/uboot-rockchip/src/arch/arm/dts/rk3399-tpm312.dts
-
 
 #开启无线功能
-# cp -f $GITHUB_WORKSPACE/configfiles/opwifi package/base-files/files/etc/init.d/opwifi
-# chmod 755 package/base-files/files/etc/init.d/opwifi
-# sed -i "s/wireless.radio\${devidx}.disabled=1/wireless.radio\${devidx}.disabled=0/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+cp -f $GITHUB_WORKSPACE/istoreos-istoreos-22.03/opwifi package/base-files/files/etc/init.d/opwifi
+chmod 755 package/base-files/files/etc/init.d/opwifi
+sed -i "s/wireless.radio\${devidx}.disabled=1/wireless.radio\${devidx}.disabled=0/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-
-#集成CPU性能跑分脚本
-cp -a $GITHUB_WORKSPACE/configfiles/coremark/* package/base-files/files/sbin/
-chmod 755 package/base-files/files/sbin/coremark
-chmod 755 package/base-files/files/sbin/coremark.sh
